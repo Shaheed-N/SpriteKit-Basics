ff009e92a7363d934049d8db1e1263a2bb7df1e2
Shaheed-N <username@example.com>
2022-12-24 15:37:00 +0400

    NEW COMMIT

--- ProjectGame/ContentView.swift
+++ ProjectGame/ContentView.swift
@@ -1,21 +1,46 @@
 //
 //  ContentView.swift
-//  ProjectGame
+//  game
 //
-//  Created by Shahid on 24.12.22.
+//  Created by Shahid on 13.09.22.
 //
 
 import SwiftUI
-
+import SpriteKit
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    class GameScene: SKScene {
+
+        let background = SKSpriteNode(imageNamed: "game")
+        let character = SKSpriteNode(imageNamed: "spider")
+        let move = SKAction.moveTo(y: 270, duration: 2)
+        let mov = SKAction.moveTo(x: 270, duration: 2)
+        let m = SKAction.moveTo(x: 100, duration: 2)
+        let mo = SKAction.moveTo(y: 100, duration: 2)
+
+
+
+
+        override func sceneDidLoad() {
+
+            background.scale(to: CGSize(width: 700, height: 500))
+            background.position = CGPoint(x: size.width / 2, y: size.width / 4)
+            background.zPosition = 2
+            addChild(background)
+            character.scale(to: CGSize(width: 100, height: 50))
+            character.position = CGPoint(x: size.width / 1.3, y: size.width / 3.2)
+            character.zPosition = 10
+            addChild(character)
+
+            let actions = SKAction.sequence([move , mov , m , mo])
+            let repeatForever = SKAction.repeatForever(actions)
+            character.run(repeatForever)
+
         }
-        .padding()
+    }
+    var body: some View {
+        SpriteView(scene: GameScene(size: CGSize(width: 500, height: 300)))
+            .ignoresSafeArea()
+
     }
 }
 
