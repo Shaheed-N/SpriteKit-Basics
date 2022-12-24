cb11bdcd9b4a8ffce20b3c8ddc144c26ef22a261
Shaheed-N <username@example.com>
2022-12-24 15:33:38 +0400

    Initial Commit

---
+++ ProjectGame/ProjectGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectGameApp.swift
+//  ProjectGame
+//
+//  Created by Shahid on 24.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
